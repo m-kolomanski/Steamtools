3acec2e11c73ec8a43c093b193ce21b3392f7737
m-kolomanski <m.kolomanski@yahoo.com>
2024-05-12 11:39:15 +0200

    Added datetime print at the start of the script, updated .gitignore

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 from checker import Checker
 from mailer import Mailer
 import sys, os, json
+from datetime import datetime
 
 if __name__ == "__main__":
-    if not os.path.exists("config.json"):
-        print("Configration file not found. You can download a template from the repository.\nRename template file to 'config.json' to start.")
-        exit()
+    try:
+        time_string = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'Running Steam new achievements checker at {time_string}')
 
-    checker = Checker(sys.argv[1:])
-    checker_res = checker.runPipeline()
+        if not os.path.exists("config.json"):
+            print("Error: Configration file not found. You can download a template from the repository.\nRename template file to 'config.json' to start.")
+            exit()
 
-    if not checker_res:
-        print("Finishing, no email to send.")
-        exit()
+        checker = Checker(sys.argv[1:])
+        checker_res = checker.runPipeline()
 
-    print("Check found new achievements, emails will be sent:")
-    print(checker_res)
+        if not checker_res:
+            print("Finishing, no email to send.")
+            exit()
 
-    with open("config.json") as config_file:
-        config = json.load(config_file)
+        print("Check found new achievements, emails will be sent:")
+        print(checker_res)
 
-    mailer = Mailer(config['general']['provider_email_adress'])
+        with open("config.json") as config_file:
+            config = json.load(config_file)
 
-    for email, games_with_achievements in checker_res.items():
-        resp = mailer.sendMessage(email, games_with_achievements)
-    
-    print("Script finished correctly")
+        mailer = Mailer(config['general']['provider_email_adress'])
+
+        for email, games_with_achievements in checker_res.items():
+            resp = mailer.sendMessage(email, games_with_achievements)
+        
+        print("Script finished correctly.")
+    except Exception:
+        import traceback
+        print(traceback.format_exc())
